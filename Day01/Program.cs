dff8cc7f486354b39f07b8be1a0a6613dd3c763d
makevs <>
2024-12-01 07:27:43 +0200

    Day 1

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,9 +1,47 @@
 ﻿namespace Day01;
 
-class Program
+internal class Program
 {
-    static void Main(string[] args)
+    protected static string FileContent => File.ReadAllText("day01.txt");
+    private static List<int> _right = new List<int>();
+    private static List<int> _left = new List<int>();
+    private static double _distance = 0;
+    private static double _similarity = 0;
+    
+    private static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        NumbersToList();
+        CalculateDistance();
+        Console.WriteLine(_distance);
+        SimilarityScore();
+        Console.WriteLine(_similarity);
+    }
+    
+    private static void NumbersToList()
+    {
+        FileContent.Split("\n").ToList().ForEach(var =>
+        {
+            var split = var.Split("   ");
+            _left.Add(int.Parse(split[0]));
+            _right.Add(int.Parse(split[1]));
+        });
+        _left.Sort();
+        _right.Sort();
+    }
+    
+    private static void CalculateDistance()
+    {
+        for (int i = 0; i < _left.Count; i++)
+        {
+            _distance += Math.Abs(_left[i] - _right[i]);
+        }
+    }
+    
+    private static void SimilarityScore()
+    {
+        foreach (int i in _left)
+        {
+            _similarity += (i * _right.Count(x => x == i));
+        }
     }
 }
